236ea2b3b8279e3ce2cc311a61aad1a38a53495b
Edward Halim <edward1@uw.edu>
2018-11-07 17:10:02 -0800

    map coord

--- eplot.R
+++ eplot.R
@@ -3,5 +3,6 @@ library(ggplot2)
 data <- read.delim("Desktop/collab_test/uw-elevations.csv.bz2")
 
 ggplot(data) + 
-  geom_point(aes(lon, lat, col=elevation)) + 
-  scale_color_gradientn(colors=terrain.colors(10))
+  geom_point(aes(Longitude, Lattitude, col=Elevation)) + 
+  scale_color_gradientn(colors=terrain.colors(10)) +
+  coord_quickmap()
